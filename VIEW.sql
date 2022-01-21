739f98274781cb39611acdbaa0275e1b243d3a67
moszynMTS <95504374+moszynMTS@users.noreply.github.com>
2022-01-21 15:53:11 +0100

    Update VIEW.sql

--- VIEW.sql
+++ VIEW.sql
@@ -60,6 +60,15 @@ INNER JOIN adres ON jednostka.id_adres=adres.id_adres
 ORDER BY adres.miasto 
 WITH READ ONLY;
 
+CREATE OR REPLACE VIEW komendanci AS
+SELECT stanowisko.nazwa, osoba.imie, osoba.nazwisko, pracownik.id_jednostka FROM stanowisko
+INNER JOIN pracownik ON stanowisko.id_stanowisko=pracownik.id_stanowisko
+INNER JOIN osoba ON osoba.id_osoby=pracownik.id_osoba
+INNER JOIN jednostka ON jednostka.id_jednostka=pracownik.id_jednostka
+WHERE stanowisko.id_stanowisko LIKE '1'
+WITH READ ONLY;
+SELECT * FROM komendanci;
+
 SELECT * FROM jurysdykcje1;
 SELECT * FROM zgloszenie1;
 SELECT * FROM pojazd1;
