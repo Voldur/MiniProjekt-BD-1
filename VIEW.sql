eda8b17497677ae0fb4ca309fb7eaf856485b5d5
moszynMTS <95504374+moszynMTS@users.noreply.github.com>
2022-01-21 15:24:55 +0100

    Update VIEW.sql
    +osoby1

--- VIEW.sql
+++ VIEW.sql
@@ -40,6 +40,12 @@ INNER JOIN osoba ON osoba.id_osoby=pracownik.id_osoba
 WHERE pojazd.id_pojazd=6
 WITH READ ONLY;
 
+CREATE OR REPLACE VIEW osoby1 AS
+SELECT osoba.id_osoby, osoba.imie,osoba.nazwisko, pracownik.pensja, adres.miasto, adres.ulica, adres.nr FROM pracownik
+INNER JOIN osoba ON pracownik.id_osoba=osoba.id_osoby
+INNER JOIN adres ON osoba.id_adres=adres.id_adres
+WITH READ ONLY;
+
 
 SELECT * FROM pojazd1;
 SELECT * FROM pojazd2;
@@ -47,3 +53,4 @@ SELECT * FROM pojazd3;
 SELECT * FROM pojazd4;
 SELECT * FROM pojazd5;
 SELECT * FROM pojazd6;
+SELECT * FROM osoby1;
