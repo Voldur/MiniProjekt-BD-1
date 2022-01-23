118e53e3bf1451337a0afcb014351ca30d898a44
Voldur <47321610+Voldur@users.noreply.github.com>
2022-01-23 18:56:29 +0100

    Update CURSOR.sql

--- CURSOR.sql
+++ CURSOR.sql
@@ -27,21 +27,23 @@ Create or replace function cursor2
 RETURN NUMBER
 IS
 CURSOR cur2 IS
-    SELECT osoba.imie, osoba.nazwisko, pracownik.pensja from osoba
-    inner join pracownik on osoba.id_osoby=pracownik.id_osoba
-    Where pracownik.id_stanowisko=1;
+    SELECT pracownik.pensja, adres.miasto from pracownik
+    inner join osoba on pracownik.id_osoba = osoba.id_osoby
+    inner join adres on adres.id_adres = osoba.id_adres
+    Where adres.miasto='Warszawa';
+srednia NUMBER :=0;
 l_prac NUMBER :=0;
 buffor cur2%rowtype;
 BEGIN
 OPEN cur2;
 LOOP
     FETCH cur2 INTO buffor;
-    EXIT WHEN cur2%NOTFOUND;
+    EXIT WHEN cur1%NOTFOUND;
     l_prac := l_prac + 1;
-    UPDATE pracownik SET pensja=pensja*2 WHERE id_stanowisko=1;
+    srednia := srednia + buffor.pensja;
 END LOOP;
 CLOSE cur2;
-RETURN l_prac;
+RETURN srednia/l_prac;
 END;
 /
 
@@ -49,42 +51,22 @@ Create or replace function cursor3
 RETURN NUMBER
 IS
 CURSOR cur3 IS
-    SELECT osoba.imie, osoba.nazwisko, pracownik.pensja from osoba
-    inner join pracownik on osoba.id_osoby=pracownik.id_osoba
-    Where pracownik.id_stanowisko=5;
+    SELECT pracownik.pensja, adres.miasto from pracownik
+    inner join osoba on pracownik.id_osoba = osoba.id_osoby
+    inner join adres on adres.id_adres = osoba.id_adres
+    Where adres.miasto='Radom';
+srednia NUMBER :=0;
 l_prac NUMBER :=0;
 buffor cur3%rowtype;
 BEGIN
 OPEN cur3;
 LOOP
     FETCH cur3 INTO buffor;
-    EXIT WHEN cur3%NOTFOUND;
+    EXIT WHEN cur1%NOTFOUND;
     l_prac := l_prac + 1;
-    UPDATE pracownik SET pensja=pensja/2 WHERE id_stanowisko=5;
+    srednia := srednia + buffor.pensja;
 END LOOP;
 CLOSE cur3;
-RETURN l_prac;
-END;
-/
-
-Create or replace function cursor4
-RETURN NUMBER
-IS
-CURSOR cur4 IS
-    SELECT osoba.imie, osoba.nazwisko, pracownik.pensja from osoba
-    inner join pracownik on osoba.id_osoby=pracownik.id_osoba
-    Where pracownik.id_pojazd=1;
-l_prac NUMBER :=0;
-buffor cur4%rowtype;
-BEGIN
-OPEN cur4;
-LOOP
-    FETCH cur4 INTO buffor;
-    EXIT WHEN cur4%NOTFOUND;
-    l_prac := l_prac + 1;
-    UPDATE pracownik SET pensja=pensja+1000 WHERE id_pojazd=1;
-END LOOP;
-CLOSE cur4;
-RETURN l_prac;
+RETURN srednia/l_prac;
 END;
 /
