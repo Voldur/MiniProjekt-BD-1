bb3082efa44dc08188d384b83596031efac1e132
Voldur <47321610+Voldur@users.noreply.github.com>
2022-01-23 19:19:35 +0100

    Update projekt DROP & CREATE & TRIGGER.sql

--- projekt DROP & CREATE & TRIGGER.sql
+++ projekt DROP & CREATE & TRIGGER.sql
@@ -127,58 +127,39 @@ CONSTRAINT ID_adres_zgl
     REFERENCES adres(ID_adres)
 );
 
-CREATE OR REPLACE TRIGGER pracownik_add
-BEFORE INSERT ON pracownik FOR EACH ROW 
+create or replace TRIGGER pracownik_add
+BEFORE INSERT ON pracownik
 DECLARE
 i NUMBER;
 BEGIN
-    SELECT COUNT(*) INTO i FROM Pracownik WHERE Pracownik.ID_stanowisko =: NEW.ID_stanowisko;
-    IF(i >= 1) THEN
+    SELECT COUNT(*) INTO i FROM Pracownik WHERE Pracownik.ID_stanowisko = 1;
+    IF(i > 1) THEN
     RAISE_APPLICATION_ERROR(-2000, 'Komendant moze byc tylko jeden');
 END IF;
 END;
-/
 
-CREATE OR REPLACE TRIGGER status_date
-AFTER INSERT OR UPDATE ON status FOR EACH ROW 
+CREATE OR REPLACE TRIGGER pensja
+AFTER INSERT OR UPDATE ON pracownik FOR EACH ROW
 DECLARE
-i NUMBER;
-max_stat NUMBER;
-data_roz DATE;
-data_zak DATE;
+pen NUMBER;
 BEGIN
-LOOP
-SELECT MAX(status.id_status) into max_stat FROM status; 
-    EXIT WHEN i > max_stat;
-    SELECT data_rozpoczecia INTO data_roz FROM status;
-    SELECT data_zakonczenia INTO data_zak FROM status;
-    if(data_roz > data_zak)
-    THEN
-    RAISE_APPLICATION_ERROR(-2001, 'Co, zgloszenie sie skonczylo jeszcze zanim sie zaczelo?');
+    Pen:=:New.pensja;
+    if(pen<3500) THEN
+        RAISE_APPLICATION_ERROR(-2001, 'Za malo placisz...');
 END IF;
-    i := i + 1;
-END LOOP;
 END;
 /
 
-CREATE OR REPLACE TRIGGER pojazd_miejsca
-AFTER INSERT ON pracownik FOR EACH ROW 
+create or replace TRIGGER pojazd_miejsca
+BEFORE INSERT ON pracownik FOR EACH ROW
 DECLARE
-max_poja NUMBER;
 prac_sel NUMBER;
 poja_sel NUMBER;
-i NUMBER;
 BEGIN
-LOOP
-SELECT MAX(pojazd.id_pojazd) into max_poja FROM pojazd;
-    EXIT WHEN i > max_poja;
-    SELECT COUNT(id_pojazd) INTO prac_sel from pracownik WHERE id_pojazd=i;
-    SELECT miejsca INTO poja_sel FROM pojazd WHERE id_pojazd=i;
-    IF (prac_sel>poja_sel)
-        THEN
+    SELECT COUNT(id_pojazd) INTO prac_sel FROM pracownik WHERE pracownik.id_pojazd =: NEW.id_pojazd;
+    SELECT miejsca INTO poja_sel FROM pojazd WHERE pojazd.id_pojazd =: NEW.id_pojazd;
+
+    IF (prac_sel>=poja_sel) THEN
         RAISE_APPLICATION_ERROR(-2002, 'Cos za duzo tych osob w tym pojezdzie');
     END IF;
-    i := i + 1;
-END LOOP;
 END;
-/
