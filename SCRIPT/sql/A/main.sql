5cfb15238b949d399b4293cc4d03855899466cba
Voldur <47321610+Voldur@users.noreply.github.com>
2022-01-24 17:57:36 +0100

    Add files via upload

---
+++ SCRIPT/sql/A/main.sql
@@ -0,0 +1,13 @@
+$CLS
+PROMPT Menu:
+PROMPT 1. Kursor Kielce;
+PROMPT 2. Kursor Warszawa;
+PROMPT 3. Kursor Radom;
+ACCEPT tmp CHAR FORMAT 'A1' DEFAULT '0' PROMPT '> '
+$CLS
+DEFINE CURR_DIR=&1
+PROMPT tmp = &tmp
+@&CURR_DIR\&tmp.sql
+PROMPT # ENTER #
+PAUSE
+@main.sql
