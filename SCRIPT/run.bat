6f7c851e86941842abbc6293747e2be3e26bdb5c
Voldur <47321610+Voldur@users.noreply.github.com>
2022-01-24 17:56:23 +0100

    Delete SCRIPT directory

--- SCRIPT/run.bat
+++
@@ -1,6 +0,0 @@
-chcp 65001
-cd sql
-sqlplus system/bd@localhost/XE @main.sql
-cd ..
-pause
-cls
--- SCRIPT/sql/6/3.sql
+++
@@ -1,9 +0,0 @@
-PROMPT
-SET ECHO ON
-CREATE OR REPLACE VIEW osoby1 AS
-SELECT osoba.id_osoby, osoba.imie,osoba.nazwisko, pracownik.pensja, adres.miasto, adres.ulica, adres.nr FROM pracownik
-INNER JOIN osoba ON pracownik.id_osoba=osoba.id_osoby
-INNER JOIN adres ON osoba.id_adres=adres.id_adres
-WITH READ ONLY;
-SET ECHO OFF
-PROMPT
